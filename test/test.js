56ffa43d436ef9e81f5a581f163caa5159a4f171
ijse <i@ijser.cn>
2015-04-03 16:41:33 +0800

    add some more tests

--- test/test.js
+++ test/test.js
@@ -10,7 +10,30 @@ function create(command) {
   return Lib.createServer(program);
 }
 
-describe('Test Serve2 lib', function() {
+describe('Test serve2 lib with all default', function() {
+  var server = create([]);
+  it('use current folder as default', function(done) {
+    request(server)
+      .get('')
+      .expect(200)
+      .expect(/bin/)
+      .expect(/lib/)
+      .expect(/test/)
+      .expect(/Readme\.md/)
+      .expect(/package.json/)
+      .end(done);
+  });
+
+  it('response one static file', function(done) {
+    request(server)
+      .get('/package.json')
+      .expect(200)
+      .expect(/version/)
+      .end(done);
+  });
+});
+
+describe('Test Serve2 lib with mock file on 9876 port', function() {
 
   var server = create([ , ,'-M', 'mock', '-p', '9876', './test/test-res']);
   it('support static file', function(done) {
@@ -32,6 +55,13 @@ describe('Test Serve2 lib', function() {
       .expect(/test\.json/)
       .expect(/text/)
       .end(done)
+  });
+
+  it('got 404 when request a file not exists', function(done) {
+    request(server)
+      .get('/file-dont-exist')
+      .expect(404)
+      .end(done);
   });
 
   it('support directory in json', function(done) {
